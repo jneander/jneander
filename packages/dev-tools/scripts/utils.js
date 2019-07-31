72f733a88a3f461a8c9acb61ee0db0ee5395d92a
Jeremy Neander <jeremy.neander@gmail.com>
2019-07-30 19:39:36 -0500

    fix(dev-tools): use cwd when resolving bin files

--- packages/dev-tools/scripts/utils.js
+++ packages/dev-tools/scripts/utils.js
@@ -24,30 +24,36 @@
 
 const fs = require('fs')
 const path = require('path')
+
 const which = require('which')
 const spawn = require('cross-spawn')
 
 function resolveBin(modName, {executable = modName, cwd = process.cwd()} = {}) {
   let pathFromWhich
+
   try {
     pathFromWhich = fs.realpathSync(which.sync(executable))
   } catch (_error) {
     // ignore _error
   }
+
   try {
-    const modPkgPath = require.resolve(`${modName}/package.json`)
+    const modPkgPath = require.resolve(`${modName}/package.json`, {paths: [cwd]})
     const modPkgDir = path.dirname(modPkgPath)
     const {bin} = require(modPkgPath)
     const binPath = typeof bin === 'string' ? bin : bin[executable]
     const fullPathToBin = path.join(modPkgDir, binPath)
+
     if (fullPathToBin === pathFromWhich) {
       return executable
     }
+
     return fullPathToBin.replace(cwd, '.')
   } catch (error) {
     if (pathFromWhich) {
       return executable
     }
+
     throw error
   }
 }
