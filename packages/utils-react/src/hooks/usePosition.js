6e72e5e2db6b57347c49d342818a8cee7ed3fa22
Jeremy Neander <jeremy.neander@gmail.com>
2019-10-02 13:49:54 -0500

    refactor(utils-react): use ref effect hook in use position

--- packages/utils-react/src/hooks/usePosition.js
+++ packages/utils-react/src/hooks/usePosition.js
@@ -1,24 +1,12 @@
-import {useLayoutEffect, useRef} from 'react'
 import Popper from 'popper.js'
+
+import {useRefEffect} from '.'
 
 export default function usePosition(options) {
   const {anchorRef, contentRef} = options
 
-  const anchorRefMemo = useRef(anchorRef.current)
-  const contentRefMemo = useRef(contentRef.current)
-  const teardownMemo = useRef(() => {})
-
-  return useLayoutEffect(() => {
-    if (
-      anchorRef.current === anchorRefMemo.current &&
-      contentRef.current === contentRefMemo.current
-    ) {
-      return
-    }
-
+  return useRefEffect(() => {
     if (!anchorRef.current || !contentRef.current) {
-      teardownMemo.current()
-      teardownMemo.current = () => {}
       return
     }
 
@@ -40,8 +28,8 @@ export default function usePosition(options) {
       placement: 'bottom-end'
     })
 
-    teardownMemo.current = () => {
+    return () => {
       popper.destroy()
     }
-  })
+  }, [anchorRef, contentRef])
 }
