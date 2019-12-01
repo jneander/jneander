2bce6a30b3e8a1e0998ac8dd1721b81ff7cf42ca
Jeremy Neander <jeremy.neander@gmail.com>
2019-12-01 14:41:45 -0600

    fix(utils-async): use global timer functions by default

--- packages/utils-async/src/waitForCondition.js
+++ packages/utils-async/src/waitForCondition.js
@@ -1,27 +1,35 @@
 const DEFAULT_INTERVAL = 0
 const DEFAULT_TIMEOUT = 1000 // ms
 
+const GLOBALS = {
+  clearInterval,
+  clearTimeout,
+  setInterval,
+  setTimeout
+}
+
 export default async function waitForCondition(conditionFn, options = {}) {
   return new Promise((resolve, reject) => {
     const intervalDuration = options.interval || DEFAULT_INTERVAL
     const timeoutDuration = options.timeout || DEFAULT_TIMEOUT
+    const globals = options.globals || GLOBALS
 
     let timeoutId
 
     const intervalFn = () => {
       const result = conditionFn()
       if (result) {
-        clearInterval(intervalId)
-        clearTimeout(timeoutId)
+        globals.clearInterval(intervalId)
+        globals.clearTimeout(timeoutId)
         resolve(result)
       }
     }
-    const intervalId = setInterval(intervalFn, intervalDuration)
+    const intervalId = globals.setInterval(intervalFn, intervalDuration)
 
     const timeoutFn = () => {
-      clearInterval(intervalId)
+      globals.clearInterval(intervalId)
       reject(new Error('Timeout waiting for condition'))
     }
-    timeoutId = setTimeout(timeoutFn, timeoutDuration)
+    timeoutId = globals.setTimeout(timeoutFn, timeoutDuration)
   })
 }
